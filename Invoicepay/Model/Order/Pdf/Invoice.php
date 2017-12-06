6c3e4089192c7e36e3ac4bfa61f9c79724e6fdf7
Fullstack World <fullstacktoworld@outlook.com>
2017-12-06 11:29:12 +0100

    customize the invocie pdf

---
+++ Invoicepay/Model/Order/Pdf/Invoice.php
@@ -0,0 +1,514 @@
+<?php
+class Pro_Invoicepay_Model_Order_Pdf_Invoice extends Pro_Pdfinvoice_Model_Order_Pdf_Invoice
+{
+    public function getPdf($invoices = array(), $pastDue = 0)
+    {
+        $this->_beforeGetPdf();
+        $this->_initRenderer('invoice');
+
+        $pdf = new Zend_Pdf();
+        $this->_setPdf($pdf);
+        $style = new Zend_Pdf_Style();
+        $this->_setFontBold($style, 10);
+
+        foreach ($invoices as $invoice) {
+            if ($invoice->getStoreId()) {
+                Mage::app()->getLocale()->emulate($invoice->getStoreId());
+                Mage::app()->setCurrentStore($invoice->getStoreId());
+            }
+            $page  = $this->newPage();
+            $order = $invoice->getOrder();
+            
+            if ($pastDue == 120 || $pastDue == 90 || $pastDue == 60) {
+                $this->insertPastDueTitle($page, $pastDue);
+            }
+            
+            /* Add image */
+            $this->insertLogo($page, $invoice->getStore());
+            /* Add address */
+            $this->insertAddress($page, $invoice->getStore());
+            /* Add head */
+            $this->insertOrder(
+                $page,
+                $order,
+                Mage::getStoreConfigFlag(self::XML_PATH_SALES_PDF_INVOICE_PUT_ORDER_ID, $order->getStoreId())
+            );
+            /* Add document text and number */
+            $this->insertDocumentTitle(
+                $page,
+                Mage::helper('sales')->__('INVOICE')
+            );
+            /* Add table */
+            $this->_drawHeader($page);
+            /* Add body */
+            foreach ($invoice->getAllItems() as $item){
+                if ($item->getOrderItem()->getParentItem()) {
+                    continue;
+                }
+                /* Draw item */
+                $this->_drawItem($item, $page, $order);
+                $page = end($pdf->pages);
+            }
+            /* Add totals */
+            $this->insertTotals($page, $invoice);
+            /* Insert footer */
+            $this->insertFooter($page);
+            if ($invoice->getStoreId()) {
+                Mage::app()->getLocale()->revert();
+            }
+        }
+        $this->_afterGetPdf();
+        return $pdf;
+    }
+    
+    /**
+     * Insert logo to pdf page
+     *
+     * @param Zend_Pdf_Page $page
+     * @param null $store
+     */
+    protected function insertLogo(&$page, $store = null)
+    {
+        $this->y = $this->y ? $this->y : 815;
+        $image = Mage::getStoreConfig('sales/identity/logo', $store);
+        if ($image) {
+            $image = Mage::getBaseDir('media') . '/sales/store/logo/' . $image;
+            if (is_file($image)) {
+                $image       = Zend_Pdf_Image::imageWithPath($image);
+                $top         = 810; //top border of the page
+                $widthLimit  = 270; //half of the page width
+                $heightLimit = 270; //assuming the image is not a "skyscraper"
+                $width       = $image->getPixelWidth();
+                $height      = $image->getPixelHeight();
+
+                //preserving aspect ratio (proportions)
+                $ratio = $width / $height;
+                if ($ratio > 1 && $width > $widthLimit) {
+                    $width  = $widthLimit;
+                    $height = $width / $ratio;
+                } elseif ($ratio < 1 && $height > $heightLimit) {
+                    $height = $heightLimit;
+                    $width  = $height * $ratio;
+                } elseif ($ratio == 1 && $height > $heightLimit) {
+                    $height = $heightLimit;
+                    $width  = $widthLimit;
+                }
+
+                $y1 = $top - $height;
+                $y2 = $top;
+                $x1 = 25;
+                $x2 = $x1 + $width;
+
+                //coordinates after transformation are rounded by Zend
+                $page->drawImage($image, $x1, $y1, $x2, $y2);
+
+                $this->y = $y1 - 10;
+            }
+        }
+    }
+    
+    public function insertDocumentTitle(Zend_Pdf_Page $page, $text)
+    {
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+        $this->_setCustomFont($page, 20);
+        $docHeader = $this->getDocHeaderCoordinates();
+        $page->drawText($text, 35, $docHeader[1] - 25, 'UTF-8');
+    }
+    
+    protected function _setCustomFont($object, $size = 7)
+    {
+        $font = Zend_Pdf_Font::fontWithName(Zend_Pdf_Font::FONT_HELVETICA_BOLD);
+        $object->setFont($font, $size);
+        return $font;
+    }
+    
+    protected function insertOrder(&$page, $obj, $putOrderId = true)
+    {
+        if ($obj instanceof Mage_Sales_Model_Order) {
+            $shipment = null;
+            $order = $obj;
+        } elseif ($obj instanceof Mage_Sales_Model_Order_Shipment) {
+            $shipment = $obj;
+            $order = $shipment->getOrder();
+        }
+
+        $this->y = $this->y ? $this->y : 815;
+        $top = $this->y;
+
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(0.45));
+        $page->setLineColor(new Zend_Pdf_Color_GrayScale(0.45));
+        $page->drawRectangle(25, $top, 570, $top - 65);
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+        $this->setDocHeaderCoordinates(array(25, $top, 570, $top - 55));
+        $this->_setFontRegular($page, 10);
+
+        if ($putOrderId) {
+            $page->drawText(
+                Mage::helper('sales')->__('Order # ') . $order->getRealOrderId(), 35, ($top -= 40), 'UTF-8'
+            );
+        }
+        $page->drawText(
+            Mage::helper('sales')->__('Order Date: ') . Mage::helper('core')->formatDate(
+                $order->getCreatedAtStoreDate(), 'medium', false
+            ),
+            35,
+            ($top -= 15),
+            'UTF-8'
+        );
+
+        $top -= 10;
+        $page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+        $page->setLineColor(new Zend_Pdf_Color_GrayScale(0.5));
+        $page->setLineWidth(0.5);
+        $page->drawRectangle(25, $top, 275, ($top - 25));
+        $page->drawRectangle(275, $top, 570, ($top - 25));
+
+        /* Calculate blocks info */
+
+        /* Billing Address */
+        $billingAddress = $this->_formatAddress($order->getBillingAddress()->format('pdf'));
+
+        /* Payment */
+        $paymentInfo = Mage::helper('payment')->getInfoBlock($order->getPayment())
+            ->setIsSecureMode(true)
+            ->toPdf();
+        $paymentInfo = htmlspecialchars_decode($paymentInfo, ENT_QUOTES);
+        $payment = explode('{{pdf_row_separator}}', $paymentInfo);
+        foreach ($payment as $key=>$value){
+            if (strip_tags(trim($value)) == '') {
+                unset($payment[$key]);
+            }
+        }
+        reset($payment);
+
+        /* Shipping Address and Method */
+        if (!$order->getIsVirtual()) {
+            /* Shipping Address */
+            $shippingAddress = $this->_formatAddress($order->getShippingAddress()->format('pdf'));
+            $shippingMethod  = $order->getShippingDescription();
+        }
+
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+        $this->_setFontBold($page, 12);
+        $page->drawText(Mage::helper('sales')->__('Sold to:'), 35, ($top - 15), 'UTF-8');
+
+        if (!$order->getIsVirtual()) {
+            $page->drawText(Mage::helper('sales')->__('Ship to:'), 285, ($top - 15), 'UTF-8');
+        } else {
+            $page->drawText(Mage::helper('sales')->__('Payment Method:'), 285, ($top - 15), 'UTF-8');
+        }
+
+        $addressesHeight = $this->_calcAddressHeight($billingAddress);
+        if (isset($shippingAddress)) {
+            $addressesHeight = max($addressesHeight, $this->_calcAddressHeight($shippingAddress));
+        }
+
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+        $page->drawRectangle(25, ($top - 25), 570, $top - 33 - $addressesHeight);
+        $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+        $this->_setFontRegular($page, 10);
+        $this->y = $top - 40;
+        $addressesStartY = $this->y;
+
+        foreach ($billingAddress as $value){
+            if ($value !== '') {
+                $text = array();
+                foreach (Mage::helper('core/string')->str_split($value, 45, true, true) as $_value) {
+                    $text[] = $_value;
+                }
+                foreach ($text as $part) {
+                    $page->drawText(strip_tags(ltrim($part)), 35, $this->y, 'UTF-8');
+                    $this->y -= 15;
+                }
+            }
+        }
+
+        $addressesEndY = $this->y;
+
+        if (!$order->getIsVirtual()) {
+            $this->y = $addressesStartY;
+            foreach ($shippingAddress as $value){
+                if ($value!=='') {
+                    $text = array();
+                    foreach (Mage::helper('core/string')->str_split($value, 45, true, true) as $_value) {
+                        $text[] = $_value;
+                    }
+                    foreach ($text as $part) {
+                        $page->drawText(strip_tags(ltrim($part)), 285, $this->y, 'UTF-8');
+                        $this->y -= 15;
+                    }
+                }
+            }
+
+            $addressesEndY = min($addressesEndY, $this->y);
+            $this->y = $addressesEndY;
+
+            $page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+            $page->setLineWidth(0.5);
+            $page->drawRectangle(25, $this->y, 275, $this->y-25);
+            $page->drawRectangle(275, $this->y, 570, $this->y-25);
+
+            $this->y -= 15;
+            $this->_setFontBold($page, 12);
+            $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+            $page->drawText(Mage::helper('sales')->__('Payment Method'), 35, $this->y, 'UTF-8');
+            $page->drawText(Mage::helper('sales')->__('Shipping Method:'), 285, $this->y , 'UTF-8');
+
+            $this->y -=10;
+            $page->setFillColor(new Zend_Pdf_Color_GrayScale(1));
+
+            $this->_setFontRegular($page, 10);
+            $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+
+            $paymentLeft = 35;
+            $yPayments   = $this->y - 15;
+        }
+        else {
+            $yPayments   = $addressesStartY;
+            $paymentLeft = 285;
+        }
+
+        foreach ($payment as $value){
+            if (trim($value) != '') {
+                //Printing "Payment Method" lines
+                $value = preg_replace('/<br[^>]*>/i', "\n", $value);
+                foreach (Mage::helper('core/string')->str_split($value, 45, true, true) as $_value) {
+                    $page->drawText(strip_tags(trim($_value)), $paymentLeft, $yPayments, 'UTF-8');
+                    $yPayments -= 15;
+                }
+            }
+        }
+
+        if ($order->getIsVirtual()) {
+            // replacement of Shipments-Payments rectangle block
+            $yPayments = min($addressesEndY, $yPayments);
+            $page->drawLine(25,  ($top - 25), 25,  $yPayments);
+            $page->drawLine(570, ($top - 25), 570, $yPayments);
+            $page->drawLine(25,  $yPayments,  570, $yPayments);
+
+            $this->y = $yPayments - 15;
+        } else {
+            $topMargin    = 15;
+            $methodStartY = $this->y;
+            $this->y     -= 15;
+
+            foreach (Mage::helper('core/string')->str_split($shippingMethod, 45, true, true) as $_value) {
+                $page->drawText(strip_tags(trim($_value)), 285, $this->y, 'UTF-8');
+                $this->y -= 15;
+            }
+
+            $yShipments = $this->y;
+            $totalShippingChargesText = "(" . Mage::helper('sales')->__('Total Shipping Charges') . " "
+                . $order->formatPriceTxt($order->getShippingAmount()) . ")";
+
+            $page->drawText($totalShippingChargesText, 285, $yShipments - $topMargin, 'UTF-8');
+            $yShipments -= $topMargin + 10;
+
+            $tracks = array();
+            if ($shipment) {
+                $tracks = $shipment->getAllTracks();
+            }
+            if (count($tracks)) {
+                $page->setFillColor(new Zend_Pdf_Color_Rgb(0.93, 0.92, 0.92));
+                $page->setLineWidth(0.5);
+                $page->drawRectangle(285, $yShipments, 510, $yShipments - 10);
+                $page->drawLine(400, $yShipments, 400, $yShipments - 10);
+                //$page->drawLine(510, $yShipments, 510, $yShipments - 10);
+
+                $this->_setFontRegular($page, 9);
+                $page->setFillColor(new Zend_Pdf_Color_GrayScale(0));
+                //$page->drawText(Mage::helper('sales')->__('Carrier'), 290, $yShipments - 7 , 'UTF-8');
+                $page->drawText(Mage::helper('sales')->__('Title'), 290, $yShipments - 7, 'UTF-8');
+                $page->drawText(Mage::helper('sales')->__('Number'), 410, $yShipments - 7, 'UTF-8');
+
+                $yShipments -= 20;
+                $this->_setFontRegular($page, 8);
+                foreach ($tracks as $track) {
+
+                    $CarrierCode = $track->getCarrierCode();
+                    if ($CarrierCode != 'custom') {
+                        $carrier = Mage::getSingleton('shipping/config')->getCarrierInstance($CarrierCode);
+                        $carrierTitle = $carrier->getConfigData('title');
+                    } else {
+                        $carrierTitle = Mage::helper('sales')->__('Custom Value');
+                    }
+
+                    //$truncatedCarrierTitle = substr($carrierTitle, 0, 35) . (strlen($carrierTitle) > 35 ? '...' : '');
+                    $maxTitleLen = 45;
+                    $endOfTitle = strlen($track->getTitle()) > $maxTitleLen ? '...' : '';
+                    $truncatedTitle = substr($track->getTitle(), 0, $maxTitleLen) . $endOfTitle;
+                    //$page->drawText($truncatedCarrierTitle, 285, $yShipments , 'UTF-8');
+                    $page->drawText($truncatedTitle, 292, $yShipments , 'UTF-8');
+                    $page->drawText($track->getNumber(), 410, $yShipments , 'UTF-8');
+                    $yShipments -= $topMargin - 5;
+                }
+            } else {
+                $yShipments -= $topMargin - 5;
+            }
+
+            $currentY = min($yPayments, $yShipments);
+
+            // replacement of Shipments-Payments rectangle block
+            $page->drawLine(25,  $methodStartY, 25,  $currentY); //left
+            $page->drawLine(25,  $currentY,     570, $currentY); //bottom
+            $page->drawLine(570, $currentY,     570, $methodStartY); //right
+
+            $this->y = $currentY;
+            $this->y -= 15;
+        }
+    }
+    
+    protected function insertPastDueTitle($page, $pastDue)
+    {
+        $this->y = $this->y ? $this->y : 815;
+        
+        $this->_setFontBold($page, 12);
+        $page->drawText($pastDue - 30 . ' Days Past Due Notice', 25, 820, 'UTF-8');
+        
+        $this->y -= 12;
+    }
+    
+    protected function insertFooter($page)
+    {
+        $lineBlock = array(
+            'lines'  => array(),
+            'height' => 12
+        );
+        
+        $lineBlock['lines'][] = array(
+            array(
+                'text'      => 'For your convenience, we accept the below credit cards:',
+                'feed'      => 25,
+                'align'     => 'left',
+                'font_size' => 10,
+                'font'      => 'bold'
+            ),
+        );
+        $lineBlock['lines'][] = array(
+            array(
+                'text'      => 'Mastercard, Visa, American Express & Discover',
+                'feed'      => 25,
+                'align'     => 'left',
+                'font_size' => 10,
+                'font'      => 'bold'
+            ),
+        );
+        
+        $this->y -= 60;
+        $page = $this->drawLineBlocks($page, array($lineBlock));
+        
+        $lineBlock['lines'] = array();
+        
+        $lineBlock['lines'][] = array(
+            array(
+                'text'      => 'You can fax in credit card payment to 714-276-6445 or',
+                'feed'      => 25,
+                'align'     => 'left',
+                'font_size' => 10,
+                'font'      => 'bold'
+            ),
+        );
+        $lineBlock['lines'][] = array(
+            array(
+                'text'      => 'mail in a check payment to the address blow.',
+                'feed'      => 25,
+                'align'     => 'left',
+                'font_size' => 10,
+                'font'      => 'bold'
+            ),
+        );
+        $lineBlock['lines'][] = array(
+            array(
+                'text'      => 'Please include your invoice number on the check.',
+                'feed'      => 25,
+                'align'     => 'left',
+                'font_size' => 10,
+                'font'      => 'bold'
+            ),
+        );
+        
+        $this->y -= 8;
+        $page = $this->drawLineBlocks($page, array($lineBlock));
+        
+        $lineBlock['lines'] = array();
+        
+        $lineBlock['lines'][] = array(
+            array(
+                'text'      => 'LaborLawCenter, Inc.',
+                'feed'      => 30,
+                'align'     => 'left',
+                'font_size' => 10,
+                'font'      => 'italic'
+            ),
+        );
+        $lineBlock['lines'][] = array(
+            array(
+                'text'      => '3501 West Garry Avenue',
+                'feed'      => 30,
+                'align'     => 'left',
+                'font_size' => 10,
+                'font'      => 'italic'
+            ),
+        );
+        $lineBlock['lines'][] = array(
+            array(
+                'text'      => 'Santa Ana, CA 92704',
+                'feed'      => 30,
+                'align'     => 'left',
+                'font_size' => 10,
+                'font'      => 'italic'
+            ),
+        );
+
+        $this->y -= 8;
+        $page = $this->drawLineBlocks($page, array($lineBlock));
+        
+        $this->_setFontBold($page, 11);
+        $page->drawText('Credit Card#', 278, $this->y += 100, 'UTF-8');
+        $this->y -= 1;
+        $page->setLineWidth(1);
+        $page->drawLine(338,  $this->y, 565,  $this->y);
+        
+        $this->y -= 30;
+        
+        $page->drawText('CCV Code:', 278, $this->y, 'UTF-8');
+        $page->drawText('Expire Date:', 402, $this->y, 'UTF-8');
+        
+        $this->y -= 1;
+        
+        $page->drawLine(328,  $this->y, 400,  $this->y);
+        $page->drawLine(460,  $this->y, 565,  $this->y);
+        
+        $this->y -= 20;
+        
+        $page->drawText('Federal Tax ID#', 278, $this->y, 'UTF-8');
+        $this->_setFontRegular($page, 11);
+        $page->drawText('20-143-9322', 356, $this->y, 'UTF-8');
+        
+        $lineBlock['lines'] = array();
+        
+        $lineBlock['lines'][] = array(
+            array(
+                'text'      => 'Thank you! We really appreciate your business.',
+                'feed'      => 200,
+                'align'     => 'center',
+                'font_size' => 10,
+                'font'      => 'bold'
+            ),
+        );
+        $lineBlock['lines'][] = array(
+            array(
+                'text'      => 'If you have any questions regarding this invoice/order please email accounting@laborlawcenter.com',
+                'feed'      => 80,
+                'align'     => 'center',
+                'font_size' => 10,
+                'font'      => 'bold'
+            ),
+        );
+        
+        $this->y -= 60;
+        $page = $this->drawLineBlocks($page, array($lineBlock));
+        
+        return $page;
+    }
+}
